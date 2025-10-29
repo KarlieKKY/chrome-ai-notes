84364d2fdd28a699f058408219b39c045fce431a
KarlieKKY <yanndeel@gmail.com>
2025-10-29 19:50:45 +0000

    add tag, tag group

--- sidepanel.js
+++ sidepanel.js
@@ -1,1 +1,21 @@
 console.log("heyyy");
+
+// Get all tab groups
+chrome.tabGroups.query({}, function (groups) {
+  groups.forEach((group) => {
+    console.log(`Group: ${group.title}, Color: ${group.color}`);
+
+    // Get tabs in this group
+    chrome.tabs.query({ groupId: group.id }, function (tabs) {
+      console.log(`Tabs in ${group.title}:`, tabs);
+    });
+  });
+});
+
+// Get ungrouped tabs
+chrome.tabs.query(
+  { groupId: chrome.tabGroups.TAB_GROUP_ID_NONE },
+  function (tabs) {
+    console.log("Ungrouped tabs:", tabs);
+  }
+);
